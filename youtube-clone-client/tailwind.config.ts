d2ddfc0bb3c056b23eb005194b1f4a840f0091c2
dddkum <dkumachev18@gmail.com>
2025-04-30 13:17:22 +0400

    prettier

--- youtube-clone-client/tailwind.config.ts
+++ youtube-clone-client/tailwind.config.ts
@@ -1,13 +1,13 @@
-import type {Config} from "tailwindcss";
-import {COLORS} from "./src/constants/colors.const";
+import type { Config } from 'tailwindcss'
+import { COLORS } from './src/constants/colors.const'
 
 const config: Config = {
-    content: ["./src/**/*.{ts,tsx}"],
-    theme: {
-        extend: {
-            colors: COLORS
-        },
-    },
-    plugins: [],
-};
-export default config;
+	content: ['./src/**/*.{ts,tsx}'],
+	theme: {
+		extend: {
+			colors: COLORS
+		}
+	},
+	plugins: []
+}
+export default config
