c04f27cf10fc3f2fac5d1b4a516494475040618f
dddkum <dkumachev18@gmail.com>
2025-05-07 00:11:55 +0400

    h1 or h2 optional prop for Heading component

--- youtube-clone-client/src/components/ui/Heading.tsx
+++ youtube-clone-client/src/components/ui/Heading.tsx
@@ -5,13 +5,18 @@ import { COLORS } from '@/constants/colors.const'
 interface Props {
 	children: ReactNode
 	Icon?: LucideIcon
+	isH1?: boolean
 }
 
-export function Heading({ children, Icon }: Props) {
+export function Heading({ children, Icon, isH1 = false }: Props) {
 	return (
 		<div className='flex items-center gap-2 mb-5'>
 			{Icon && <Icon color={COLORS.primary} />}
-			<h2 className='font-semibold text-lg'>{children}</h2>
+			{isH1 ? (
+				<h1 className='font-semibold text-lg'>{children}</h1>
+			) : (
+				<h2 className='font-semibold text-lg'>{children}</h2>
+			)}
 		</div>
 	)
 }
