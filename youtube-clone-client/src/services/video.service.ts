ce71b3334e47063cb61a48b8dc0744c45c4d4964
dddkum <dkumachev18@gmail.com>
2025-05-06 16:16:04 +0400

    get trending videos fn

--- youtube-clone-client/src/services/video.service.ts
+++ youtube-clone-client/src/services/video.service.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
-import type { IVideo } from '@/types/video.types'
+import type { IVideo, IVideoData } from '@/types/video.types'
 
 class VideoService {
+	getTrendingVideos() {
+		return axios.get<IVideoData[]>('http://localhost:4200/api/videos/trending')
+	}
+
 	getExploreVideos() {
 		return axios.get<IVideo>('http://localhost:4200/api/videos/explore')
 	}
