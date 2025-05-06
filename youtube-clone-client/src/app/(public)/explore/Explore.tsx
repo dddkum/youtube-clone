b2a3065c4965fe1eb2c7781075056b4554260965
dddkum <dkumachev18@gmail.com>
2025-05-06 22:12:20 +0400

    Skeleton loader to dynamic components

--- youtube-clone-client/src/app/(public)/explore/Explore.tsx
+++ youtube-clone-client/src/app/(public)/explore/Explore.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { VideoItem } from '@/ui/video-item/VideoItem'
-import { Compass, Dot} from 'lucide-react'
+import { Compass, Dot } from 'lucide-react'
 import { useQuery } from '@tanstack/react-query'
 import { videoService } from '@/services/video.service'
 import { Heading } from '@/ui/Heading'
+import { SkeletonLoader } from '@/ui/SkeletonLoader'
 
 function Explore() {
 	const { data, isLoading } = useQuery({
@@ -15,15 +16,20 @@ function Explore() {
 		<section>
 			<Heading Icon={Compass}>Explore new videos</Heading>
 			<div className='grid grid-cols-4 gap-6'>
-				{isLoading
-					? 'Loading...'
-					: data?.data.videos.map(video => (
-							<VideoItem
-								key={video.id}
-								video={video}
-								Icon={Dot}
-							/>
-						))}
+				{isLoading ? (
+					<SkeletonLoader
+						count={4}
+						className='h-55 rounded-md'
+					/>
+				) : (
+					data?.data.videos.map(video => (
+						<VideoItem
+							key={video.id}
+							video={video}
+							Icon={Dot}
+						/>
+					))
+				)}
 			</div>
 		</section>
 	)
