fb4b306f7dec5bd891ac26d21328d9a7607a248d
dddkum <dkumachev18@gmail.com>
2025-05-07 16:27:30 +0400

    toaster provider

--- youtube-clone-client/src/providers/Providers.tsx
+++ youtube-clone-client/src/providers/Providers.tsx
@@ -4,6 +4,7 @@ import type { ReactNode } from 'react'
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { domAnimation, LazyMotion } from 'framer-motion'
+import { Toaster } from 'react-hot-toast'
 
 interface Props {
 	children: ReactNode
@@ -14,7 +15,10 @@ const client = new QueryClient()
 export function Providers({ children }: Props) {
 	return (
 		<QueryClientProvider client={client}>
-			<LazyMotion features={domAnimation}>{children}</LazyMotion>
+			<LazyMotion features={domAnimation}>
+				{children}
+				<Toaster />
+			</LazyMotion>
 		</QueryClientProvider>
 	)
 }
