923c4bddccb718b5ca9bb0814a78d4bbcbb2e244
dddkum <dkumachev18@gmail.com>
2025-05-07 09:41:43 +0400

    trending page

--- youtube-clone-client/src/app/(public)/trending/page.tsx
+++ youtube-clone-client/src/app/(public)/trending/page.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next'
 import { PAGE } from '@/config/public-page.config'
+import { videoService } from '@/services/video.service'
+import { Heading } from '@/ui/Heading'
+import { Flame } from 'lucide-react'
+import { VideoItem } from '@/ui/video-item/VideoItem'
 
 export const metadata: Metadata = {
 	title: 'Trending',
@@ -10,9 +14,29 @@ export const metadata: Metadata = {
 	openGraph: {
 		type: 'website',
 		url: PAGE.TRENDING,
-		title: 'Trending',
+		title: 'Trending'
 	}
 }
-export default function TrendingPage() {
-	return <div>Trending Page</div>
+
+export const revalidate = 100
+export const dynamic = 'force-static'
+
+export default async function TrendingPage() {
+	const videos = await videoService.getTrendingVideos()
+	return (
+		<section>
+			<Heading Icon={Flame}>Trending videos</Heading>
+			<div className='grid grid-cols-4 gap-6 mb-10'>
+				{videos.data.length
+					? videos.data.map(video => (
+							<VideoItem
+								key={video.id}
+								video={video}
+								Icon={Flame}
+							/>
+						))
+					: 'Trending videos are unavailable'}
+			</div>
+		</section>
+	)
 }
